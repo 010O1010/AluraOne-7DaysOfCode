da9fdba18860d569c36cb526d9b9af8f08a4a2e3
Johann Raphael <jraphael.primo@gmail.com>
2025-02-26 21:39:50 -0300

    Add: OracleOne Alura Day01

---
+++ day1.js
@@ -0,0 +1,14 @@
+//Day 01 Challenge Alura #7DaysOfCode -- GitHub 010O1010
+
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+numeroUm == stringUm ? console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes.') : console.log('As variáveis numeroUm e string não tem o mesmo valor');
+
+numeroTrinta == stringTrinta ? console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor, mas tipos diferentes.') : console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo valor');
+
+numeroDez == numeroDez ? console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes.') : console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
